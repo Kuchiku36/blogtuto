502ab7b471bcdfe6f1fa206b3aca87a785ed86b8
veronique derick <derickveronique@gmail.com>
2024-07-03 10:33:16 -0400

    création des controller

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\Comment;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CommentController extends Controller
+{
+    public function store(Request $request, Post $post)
+    {
+        $request->validate([
+            'content'=>'required',
+        ]);
+
+        $comment = new Comment([
+            'content'=>$request->content,
+            'user_id'=>auth()->id(),
+        ]);
+
+        $post->comments()->save($comment);
+
+        return redirect()->route('posts.show', $post->id)->with('success', 'Commentaire ajouté avec succès.');
+    }
+    //
+}
